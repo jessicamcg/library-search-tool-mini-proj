48d7167292c9def705bc442424910f194a3beb36
jessicamcg <jessicamcguico@gmail.com>
2021-10-04 22:02:48 -0500

    test

---
+++ assets/results.js
@@ -0,0 +1,14 @@
+
+
+var searchForm = document.querySelector('#search-form');
+var searchInput = document.querySelector('#search-input');
+var formatInput = document.querySelector('#format-select');
+
+
+function renderResults() {
+
+    //render title
+
+    //render cards
+
+}
